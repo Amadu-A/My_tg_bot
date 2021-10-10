d186ab1935abe6c8efaeb1f5062b9ded8e7667b4
AmaduA <main_ivan@mail.ru>
2021-10-10 23:18:05 +0400

    how do it?

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import json
 import random
+import sqlite3
 
+import requests
 import telebot
 from telebot import types
 # import logging
 # import re
-# import requests
 # import json
 #
 #
 from config import BOT_TOKEN, API_KEY
-import botrequests
+#import botrequests
+from botrequests.lowprice import get_city, search_hotels
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -18,20 +21,55 @@ headers = {
         'x-rapidapi-key': API_KEY
     }
 my_url = 'https://hotels4.p.rapidapi.com/locations/search'
-
+url_lang = 'https://hotels4.p.rapidapi.com/get-meta-data'
+params = {'query': 'new york', 'locale': 'en_US'}
 help_msg = '/lowprice - самые дешёвые отели\n' \
            '/highprice - самые дорогие отели в городе\n' \
            '/bestdeal - отели, подходящие по цене и удаленности от центра\n' \
            '/history - история поиска'
 
+
+@bot.message_handler(commands=['lowprice'])
+def welcome(message):
+    # выбираем язык. Будет скрипт-меню с кнопкой
+    language = 'ru_RU'
+    bot.send_message(message.chat.id, 'Введите город')
+    # тут будет скрипт с библиотекой re, который будет искать похожие названия городов, если
+    # не обнаружится совпадений
+    bot.register_next_step_handler(message, get_city)
+    bot.send_message(message.chat.id, 'Идет поиск отелей')
+    bot.register_next_step_handler(message, search_hotels)
+
+
+    # location_dict = requests.get(my_url, headers=headers, params=params)
+    # response_langu = requests.request("GET", url_lang, headers=headers)
+
+
+
+    # with open('test.json', 'w') as file:
+    #     json.dump(json.loads(location_dict.text), file, indent=4)
+    # print(response_langu.text)
+    # with open('lang.json', 'w') as file:
+    #     json.dump(json.loads(response_langu.text), file, indent=4)
+
+
+@bot.message_handler(commands=['highprice'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Команда highprice в стадии разработки')
+
+@bot.message_handler(commands=['bestdeal'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Команда bestdeal в стадии разработки')
+
+@bot.message_handler(commands=['history'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Команда history в стадии разработки')
+
+
 @bot.message_handler(content_types=['text'])
 def get_textmessages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/hello-world":
-        bot.send_message(message.from_user.id, "Напишите: привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /hello-world")
+    bot.send_message(message.from_user.id, text=help_msg)
+
 
 
 bot.polling(none_stop=True, interval=0)

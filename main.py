a877307b8b97ee219359ed1d6cebefd0e14557f9
AmaduA <main_ivan@mail.ru>
2021-10-19 21:33:31 +0400

    some code was move into lowprice.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
 import sqlite3
 import datetime
-import requests
 import telebot
 from telebot import types
 # import logging
@@ -10,8 +8,8 @@ from telebot import types
 #
 #
 from config import BOT_TOKEN, API_KEY
-from classUser import *
-#from botrequests.lowprice import *
+# import botrequests
+from botrequests.lowprice import *
 #from botrequests.lowprice import get_hotels
 
 
@@ -24,6 +22,9 @@ url_id_city = 'https://hotels4.p.rapidapi.com/locations/search'
 url_detail = 'https://hotels4.p.rapidapi.com/properties/list'
 url_lang = 'https://hotels4.p.rapidapi.com/get-meta-data'
 params = {'query': 'new york', 'locale': 'en_US'}
+date_today = datetime.datetime.today().strftime('%Y-%m-%d')
+date_tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
 
 query_param = {
         'count_hotels': None,
@@ -35,9 +36,7 @@ help_msg = '/lowprice - самые дешёвые отели\n' \
            '/highprice - самые дорогие отели в городе\n' \
            '/bestdeal - отели, подходящие по цене и удаленности от центра\n' \
            '/history - история поиска'
-date_today = datetime.datetime.today().strftime('%Y-%m-%d')
-date_tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-print(date_today, date_tomorrow)
+
 
 @bot.message_handler(commands=['lowprice'])
 def welcome(message):
@@ -100,42 +99,6 @@ def callback_inline(call):
         print(hotel.get_hotel())
         bot.send_message(chat_id=call.message.chat.id, text=hotel.get_hotel())
 
-def get_hotels(query_param: dict):
-    querystring_search = {'query': query_param['city'], 'locale': 're_RU'}
-    response = json.loads(requests.request('GET', url_id_city, headers=headers, params=querystring_search).text)
-    print(response)
-    city_id = response.get('suggestions')[0].get('entities')[0].get('destinationId')
-    querystring_detail = {
-        'adults1': '1',
-        'pageNumber': '1',
-        'destinationId': city_id,
-        'pageSize': str(query_param['count_hotels']),
-        'checkOut': '{}'.format(date_tomorrow),
-        'checkIn': '{}'.format(date_today),
-        'sortOrder': query_param['sorting'],
-        'locale': 'ru_RU',
-        'currency': 'RUB'
-    }
-    print(querystring_detail)
-    response = json.loads(requests.request('GET', url_detail, headers=headers, params=querystring_detail).text)
-    print(response)
-    result = response.get('data').get('body').get('searchResults').get('results')
-    print(json.dumps(result, indent=4))
-    list_hotels = []
-    for hotel in result:
-        list_hotels.append(User(hotel_id=hotel.get('id'),
-                                name=hotel.get('name'),
-                                country=hotel.get('address').get('countryName'),
-                                city=hotel.get('address').get('locality'),
-                                postal_code=hotel.get('address').get('postalCode'),
-                                address=hotel.get('address').get('streetAddress'),
-                                star_rating=hotel.get('star_rating'),
-                                #distance=hotel.get('distance'),
-                                price=hotel.get('ratePlan').get('price').get('current')
-                                # место для фото
-                                #cite=hotel.get('cite')
-        ))
-    return list_hotels
 
-
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
